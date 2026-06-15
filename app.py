d08dc034d2611154310f735efe80d1a5cb2239d3
vreddy-web <vreddy@digitalocean.com>
2026-06-15 13:43:52 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,9 +1,31 @@
-from flask import Flask
-from flask import render_template
+import os
+import boto3
+from botocore.config import Config
+from flask import Flask, render_template
 
 app = Flask(__name__)
 
+def get_spaces_client():
+    return boto3.client(
+        's3',
+        endpoint_url=os.environ.get('SPACES_ENDPOINT'),
+        aws_access_key_id=os.environ.get('SPACES_KEY'),
+        aws_secret_access_key=os.environ.get('SPACES_SECRET'),
+        config=Config(signature_version='s3v4')
+    )
 
 @app.route("/")
 def hello_world():
-    return render_template("index.html")
+    image_url = None
+    error = None
+    try:
+        client = get_spaces_client()
+        bucket = os.environ.get('SPACES_BUCKET', 'nfs-app-test')
+        image_url = client.generate_presigned_url(
+            'get_object',
+            Params={'Bucket': bucket, 'Key': 'images (1).jpeg'},
+            ExpiresIn=3600
+        )
+    except Exception as e:
+        error = str(e)
+    return render_template("index.html", image_url=image_url, error=error)
